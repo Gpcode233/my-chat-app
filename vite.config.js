221ccc7764ef2dfa9b970f8e14ec5dd1fc3e080f
Gpcode233 <godspowerojini8@gmail.com>
2025-03-11 13:41:27 +0100

    Fix SCSS imports and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000, // Set to 1000 kB
   },
+  plugins: [react(), visualizer()],
   plugins: [react()],
 })
